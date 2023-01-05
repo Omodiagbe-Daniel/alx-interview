5fa1ab0946be6c01c76c0c3d7d2887e3efc7e7cc
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-05 19:48:24 +0100

    a method that determines if all the boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Lockboxes project"""
+
+
+def canUnlockAll(boxes):
+    """You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes"""
+
+    opened_boxes = [0]
+    size = len(boxes)
+    for id in range(size):
+        for box in (boxes):
+            if box == []:
+                break
+            for ele in box:
+                if ele < size and ele != id and ele not in opened_boxes:
+                    opened_boxes.append(ele)
+
+    if len(opened_boxes) == size:
+        return True
+    else:
+        return False
