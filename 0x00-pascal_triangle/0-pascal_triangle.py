eadf22d362f535995294785dcf628ed564b7c3c5
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-08 15:38:59 +0100

    implemented pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""pascal triangle implementation with python"""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    if n <= 0:
+        return [[]]
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+
+    mainlist = [[1], [1, 1]]
+    for i in range(1, n - 1):
+        list1 = [1]
+        for j in range(len(mainlist[i]) - 1):
+            list1.append(mainlist[i][j] + mainlist[i][j + 1])
+        list1.append(1)
+        mainlist.append(list1)
+    return mainlist
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,11 @@
+0x00. Pascal's Triangle
+Algorithm
+Python
+ By: Alexa Orrico, Software Engineer at Holberton School
+ Weight: 1
+ Project over - took place from Dec 19, 2022 6:00 AM to Dec 23, 2022 6:00 AM
+ An auto review will be launched at the deadline
+Concepts
+For this project, we expect you to look at this concept:
+
+Technical interview
