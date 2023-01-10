bb7db7f28b68926e3f5a15e9868ff142f421b9db
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-10 14:41:43 +0100

    minimum operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,15 +11,17 @@ def minOperations(n):
        to result in exactly n H characters in the file
     """
 
-    if sqrt(n) == int(sqrt(n)):
-        return (int(sqrt(n)) * 2)
+    if n > 0:
+        if sqrt(n) == int(sqrt(n)):
+            return (int(sqrt(n)) * 2)
 
-    n_list = []
-    for i in range(1, n):
-        if n % i == 0 and i != 1:
-            n_list.append(i)
-    len_n_list = len(n_list)
-    if len_n_list != 0:
+        n_list = []
+        for i in range(1, n):
+            if n % i == 0 and i != 1:
+                n_list.append(i)
+        len_n_list = len(n_list)
+        if len_n_list == 0:
+            return n
         if len_n_list == 2:
             return n_list[0] * 2
         min_odd = min([x for x in n_list if x & 1])
