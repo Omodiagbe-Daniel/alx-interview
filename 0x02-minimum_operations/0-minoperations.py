cbe614e5e048d2d65b778c9ed79e51cb9f932fcb
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-12 03:04:18 +0100

    made correction

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,7 +15,8 @@ def minOperations(n):
         if sqrt(n) == int(sqrt(n)):
             if (sqrt(n)) % 2 != 0:
                 return (int(sqrt(n)) * 2)
-
+        if sqrt(n) % 2 == 0:
+            return int(sqrt(n)) + 2
         n_list = []
         for i in range(1, n):
             if n % i == 0 and i != 1:
